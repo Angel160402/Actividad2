fc5a0a3f253565017a0edfe6b92a63bc1cf88d45
User <User@DESKTOP-AMQNG48>
2021-04-23 10:44:49 -0500

    Prueba de ejercicio 2

---
+++ src/actividad2/Actividad2.java
@@ -0,0 +1,51 @@
+package actividad2;
+
+/**
+ *
+ * @author Ángel Hidalgo
+ */
+public class Actividad2 {
+
+    static void encabezados() {
+        System.out.println("UNIVERSIDAD AUTÓNOMA DE CAMPECHE");
+        System.out.println("FACULTAD DE INGENIERÍA");
+        System.out.println("INGENIERÍA EN SISTEMAS COMPUTACIONALES");
+        System.out.println("----------------------------------------");
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        encabezados();
+
+        //1.- Crear un menú del 1 al 5 con opciones de acciones  se lea un valor de tipo int y se proceso algún mensaje.   
+        int valor = 0;
+
+        System.out.println("[----------EJERCICIO 1-------------]");
+        System.out.println("[-----------M E N Ú---------------]");
+
+        switch (valor) {
+
+            case 1:
+                System.out.println(""); 
+                break;
+            case 2:
+                System.out.println("");
+                break;
+            case 3:
+                System.out.println("");
+                break;
+            case 4:
+                System.out.println("");
+                break;
+            case 5:
+                System.out.println("");
+                break;
+        }
+
+    }
+
+}
