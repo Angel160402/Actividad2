a8158b528f95bef626b00bb04a04e95009cfa7b2
User <User@DESKTOP-AMQNG48>
2021-04-23 12:27:26 -0500

    Prueba 3 de Actividad 2

--- src/actividad2/Actividad2.java
+++ src/actividad2/Actividad2.java
@@ -16,6 +16,10 @@ public class Actividad2 {
 
     }
 
+    static void separador() {
+        System.out.println("[-----------------------------------------------]");
+    }
+
     static void menú() {
         System.out.println("1.-----Sandaleas / Gucci-----");
         System.out.println("2.-----Tennis / Nike-----");
@@ -38,27 +42,40 @@ public class Actividad2 {
 
         System.out.println("[----------EJERCICIO 1-------------]");
         System.out.println("[-----------M E N Ú---------------]");
-        System.out.println("Escoja un elemento de nuestro menú:");
         menú();
+        System.out.println("Escoja un elemento de nuestro menú:");
         valor = teclado.nextInt();
         switch (valor) {
 
             case 1:
                 System.out.println("Sandaleas / Gucci");
+                System.out.println("PRECIO: $3,000");
                 break;
             case 2:
                 System.out.println("Tennis / Nike");
+                System.out.println("PRECIO: $400");
                 break;
             case 3:
                 System.out.println("Tennis / Ozono");
+                System.out.println("PRECIO: $499");
                 break;
             case 4:
                 System.out.println("Zapatos / Flexi");
+                System.out.println("PRECIO: $320");
                 break;
             case 5:
                 System.out.println("Botas / Contempo");
+                System.out.println("PRECIO: $999");
                 break;
         }
+
+        separador();
+        System.out.println(" ");
+        //2.- Crear un menú del 1 al 5 con opciones de acciones  se lea un valor de tipo string 
+        //(correr, saltar, dormir, comer, estudiar) y se proceso algun mensaje.
+        String opcion = "";
+
+        System.out.println("[----------EJERCICIO 2--------------]");
 
     }
 
