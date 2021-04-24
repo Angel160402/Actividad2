ab3937fa1cc27c99f149505de45655a48b5494de
User <User@DESKTOP-AMQNG48>
2021-04-23 23:04:55 -0500

    Prueba 6 de Actividad 2

--- src/actividad2/Actividad2.java
+++ src/actividad2/Actividad2.java
@@ -47,14 +47,19 @@ public class Actividad2 {
         System.out.println("4.-----Zapatos / Flexi-----");
         System.out.println("5.-----Botas / Contempo-----");
     }
+    
+    public static void diasDeLaSemana() {
+        
+    }
 
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-
+/*
         encabezados();
         System.out.println(" ");
         //1.- Crear un menú del 1 al 5 con opciones de acciones  se lea un valor de tipo int y se procese algún mensaje.   
@@ -88,7 +93,7 @@ public class Actividad2 {
                 System.out.println("PRECIO: $999");
                 break;
         }
-         
+
         separador();
         System.out.println(" ");
         //2.- Crear un menú del 1 al 5 con opciones de acciones  se lea un valor de tipo string 
@@ -107,26 +112,26 @@ public class Actividad2 {
 
         switch (opcion) {
             case "Correr":
-                System.out.println("Usted eligio:" + " " + run);
+                System.out.println("Usted eligio:" + run);
                 break;
             case "Saltar":
-                System.out.println("Usted eligio:" + " " + jump);
+                System.out.println("Usted eligio:" + jump);
                 break;
             case "Dormir":
-                System.out.println("Usted eligio:" + " " + sleep);
+                System.out.println("Usted eligio:" + sleep);
                 break;
             case "Comer":
-                System.out.println("Usted eligio:" + " " + eat);
+                System.out.println("Usted eligio:" + eat);
                 break;
             case "Estudiar":
-                System.out.println("Usted eligio:" + " " + study);
+                System.out.println("Usted eligio:" + study);
                 break;
         }
 
         separador();
         System.out.println(" ");
-        /*3.- Crear un horario de usted de cualquier día de la semana, ej: 6 Despertar, 7 Clase LP1, 8 Clase LP1, 9 Clase Estructuras, 10 …,
-         21 Dormir, leer un valor de 1  al 24 validar e imprimir la acción que realiza en su horario.*/
+        //3.- Crear un horario de usted de cualquier día de la semana, ej: 6 Despertar, 7 Clase LP1, 8 Clase LP1, 9 Clase Estructuras, 10 …,
+        //21 Dormir, leer un valor de 1  al 24 validar e imprimir la acción que realiza en su horario.
         System.out.println("[----------EJERCICIO 3-------------]");
         System.out.println("[-----------HORARIO DEL DÍA JUEVES---------------]");
         int val1 = 0;
@@ -136,7 +141,7 @@ public class Actividad2 {
         val1 = teclado.nextInt();
 
         switch (val1) {
-            
+
             case 6:
                 System.out.println("Despertar");
                 break;
@@ -163,14 +168,25 @@ public class Actividad2 {
                 break;
             case 19:
                 System.out.println("Clase de Lenguaje de Programación");
-              break;
+                break;
             case 21:
                 System.out.println("Dormir");
-                default:
-                    System.out.println("Clase");
-
+            default:
+                System.out.println("Clase");
         }
+        */
 
+        separador();
+        System.out.println(" ");
+        /*4.- Crear una método en lea un número de tipo int y se solicite a otro método
+        que valide el dato entre el 1 al 7  si el valor esta fuera de este rango regrese 
+        que es incorrecto y el nombre del día de la semana, como segundo parámetro reciba 
+        una valor boleano donde true es semana inglesa (inicia domingo), false es semana laborable (inicia en lunes).*/
+        System.out.println("[----------EJERCICIO 4-------------]");
+        System.out.println("[----------DÍAS DE LA SEMANA-------------]");
+        
     }
 
 }
+
+
